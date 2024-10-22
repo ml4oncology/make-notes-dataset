f3c4fe4fbeaa64610f826dda5314e10324fc8eb7
wayneisaacuy <wayneisaacuy@gmail.com>
2024-10-22 19:34:07 -0400

    add bash script for merging and cleaning notes

--- scripts/merge_clean_notes.sh
+++ scripts/merge_clean_notes.sh
@@ -5,6 +5,8 @@ userName="t127556uhn"
 memory=16
 condaEnv="~/miniforge3/envs/basic/bin/python"
 nGPU=0
+run_time="1-00:00:00"
+partition="himem"
 
 dataDir="/cluster/projects/gliugroup/2BLAST/clinical_notes/HealthReportRecords/results_status_dates/processed/dataframes"
 saveDir="/cluster/projects/gliugroup/2BLAST/clinical_notes/HealthReportRecords/results_status_dates/processed/dataframes"
@@ -18,4 +20,4 @@ save_dir="/cluster/projects/gliugroup/2BLAST/data/processed/clinical_notes/data_
 file_part_max_obs=598
 file_part_max_clin=598
 
-pySLURMargs.py $userName $memory $condaEnv $nGPU "../src/merge_clean_notes.py $data_dir_obs $data_dir_clin $json_dir_obs $json_dir_clin $mrn_file $save_dir $file_part_max_obs $file_part_max_clin"
+pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/merge_clean_notes.py $data_dir_obs $data_dir_clin $json_dir_obs $json_dir_clin $mrn_file $save_dir $file_part_max_obs $file_part_max_clin"
