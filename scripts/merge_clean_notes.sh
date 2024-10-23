0632268f418c99612da1a854290e414fa3142a3e
wayneisaacuy <wayneisaacuy@gmail.com>
2024-10-23 12:34:25 -0400

    add merge and clean notes

--- scripts/merge_clean_notes.sh
+++ scripts/merge_clean_notes.sh
@@ -6,18 +6,10 @@ memory=16
 condaEnv="~/miniforge3/envs/basic/bin/python"
 nGPU=0
 run_time="1-00:00:00"
-partition="himem"
+partition="all"
 
-dataDir="/cluster/projects/gliugroup/2BLAST/clinical_notes/HealthReportRecords/results_status_dates/processed/dataframes"
-saveDir="/cluster/projects/gliugroup/2BLAST/clinical_notes/HealthReportRecords/results_status_dates/processed/dataframes"
-
-data_dir_obs="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_parquet_gzip_notes_p1"
-data_dir_clin="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_parquet_gzip_notes_p2"
-json_dir_obs="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_json_p1"
-json_dir_clin="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_json_p2"
-mrn_file="/cluster/home/t127556uhn/misc/mrn_map_2Blast_part4.csv"
-save_dir="/cluster/projects/gliugroup/2BLAST/data/processed/clinical_notes/data_pull_2024-06-04"
+parquet_gzip_dir="/cluster/projects/gliugroup/2BLAST/data/processed/clinical_notes/data_pull_2024-06-04"
 file_part_max_obs=598
 file_part_max_clin=598
 
-pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/merge_clean_notes.py $data_dir_obs $data_dir_clin $json_dir_obs $json_dir_clin $mrn_file $save_dir $file_part_max_obs $file_part_max_clin"
+pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/merge_clean_notes.py $parquet_gzip_dir $file_part_max_obs $file_part_max_clin"
--- src/merge_clean_notes.py
+++ src/merge_clean_notes.py
@@ -2,18 +2,13 @@ import pandas as pd
 import argparse
 import os
 from util import (extract_date_from_note, 
-                  extract_job_num, 
-                  parallelize,
+                  extract_job_num,
                   strip_title)
-from functools import partial
-from process_notes import process_notes
 import logging
 
 logger = logging.getLogger(__name__)
 
-def merge_clean_notes(data_dir_observations, data_dir_clinical, 
-                      json_dir_observations, json_dir_clinical,
-                      mrn_file, save_dir, file_part_max_observations,
+def merge_clean_notes(parquet_gzip_dir, file_part_max_observations,
                       file_part_max_clinical):
     """
     Process each observation notes and clinic notes and merge
@@ -21,46 +16,10 @@ def merge_clean_notes(data_dir_observations, data_dir_clinical,
     replacing the date with date in note if available and dropping 
     duplicates according to extracted job number and date last updated.
 
-    save_dir: directory path where merged parquet file will be saved
+    parquet_gzip_dir: directory path where the parquet gzip files are stored
     file_part_max_observations: maximum number of files for the observations notes
     file_part_max_clinical: maximum number of files for the clinical notes
     """
-
-    # data_dir_observations: directory path of the parquet.gzip files for observations
-    # data_dir_clinical: directory path of the parquet.gzip files for clinical
-    # json_dir_observations: directory path of the json files for observations
-    # json_dir_clinical: directory path of the json files for clinical
-    # mrn_file: mrn file
-
-    # apply parallel processing to process each dataframe for clinic (missing) 
-
-    # worker = partial(process_notes, data_dir=data_dir_clinical, 
-    #                  json_dir=json_dir_clinical, 
-    #                  save_dir=save_dir,
-    #                  mrn_file=mrn_file, 
-    #                  missing_notes=1)
-    # worker = partial(process_notes, data_dir_clinical, 
-    #                  json_dir_clinical, 
-    #                  save_dir,
-    #                  mrn_file, 
-    #                  1)
-    # generator = list(range(file_part_max_clinical + 1))
-    # out = parallelize(generator, worker, processes=os.cpu_count())
-
-    # apply parallel processing to process each dataframe for observation (non-missing)
-
-    # worker = partial(process_notes, data_dir=data_dir_observations, 
-    #                  json_dir=json_dir_observations, 
-    #                  save_dir=save_dir,
-    #                  mrn_file=mrn_file, 
-    #                  missing_notes=0)
-    # worker = partial(process_notes, data_dir_observations, 
-    #                  json_dir_observations, 
-    #                  save_dir,
-    #                  mrn_file, 
-    #                  0)
-    # generator = list(range(file_part_max_observations + 1))
-    # out = parallelize(generator, worker, processes=os.cpu_count())
 
     # merge dataframes
     merged_notes = dict()
@@ -75,7 +34,7 @@ def merge_clean_notes(data_dir_observations, data_dir_clinical,
         merged_notes_list = []
         for ctr in range(file_part_max + 1):
             # load dataframe
-            df_temp = pd.read_parquet(os.path.join(save_dir, f'{fname}_{ctr}.parquet.gzip'), 
+            df_temp = pd.read_parquet(os.path.join(parquet_gzip_dir, f'{fname}_{ctr}.parquet.gzip'), 
                                       engine='pyarrow', use_nullable_dtypes = True)
             merged_notes_list.append(df_temp)
         merged_notes[note_type] = pd.concat(merged_notes_list)
@@ -90,11 +49,11 @@ def merge_clean_notes(data_dir_observations, data_dir_clinical,
                                  'last_updated', 'dictated_by']
     merged_notes['observations'] = merged_notes['observations'][cols_to_keep_observations].copy()
 
-    cols_to_keep_clinical = ['mrn', 'note_text', 'clinical_notes', 
+    cols_to_keep_clinical = ['mrn', 'code_text', 'clinical_notes', 
                              'visit_date', 'processed_physician_name', 
                              'last_updated', 'dictated_by']
     merged_notes['clinical'] = merged_notes['clinical'][cols_to_keep_clinical].copy()
-    merged_notes['clinical'].rename(columns={'note_text':"Observations.ProcName"}, inplace=True)
+    merged_notes['clinical'].rename(columns={'code_text':"Observations.ProcName"}, inplace=True)
     notes_df = pd.concat([merged_notes['observations'], merged_notes['clinical']], ignore_index=True)
 
     # add physician name
@@ -138,22 +97,15 @@ def merge_clean_notes(data_dir_observations, data_dir_clinical,
     merged_notes_drop_duplicates = pd.concat([notes_df.loc[~notes_df['job_id'].isin(job_id_w_duplicates)], filtered_records]).reset_index()
 
     cols_to_keep = ['mrn', 'Observations.ProcName', 'processed_physician_name', 'processed_date', 'clinical_notes', 'epr_date', 'dictated_by']
-    merged_notes_drop_duplicates[cols_to_keep].to_parquet(f'{save_dir}/merged_processed_cleaned_clinical_notes.parquet.gzip', compression='gzip', index=False)
+    merged_notes_drop_duplicates[cols_to_keep].to_parquet(f'{parquet_gzip_dir}/merged_processed_cleaned_clinical_notes.parquet.gzip', compression='gzip', index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_dir_observations", help = "data directory for observations notes", type = str) # data directory for observations notes
-    parser.add_argument("data_dir_clinical", help = "data directory for clinical notes", type = str) # data directory for clinical notes
-    parser.add_argument("json_dir_observations", help = "json directory for observations notes", type = str) # json directory for observations notes
-    parser.add_argument("json_dir_clinical", help = "json directory for clinical notes", type = str) # json directory for clinical notes
-    parser.add_argument("mrn_file", help = "MRN file", type = str) # file where MRN is saved
-    parser.add_argument("save_dir", help = "save directory of merged notes", type = str) # save directory for merged notes
+    parser.add_argument("parquet_gzip_dir", help = "directory of the parquet gzip files", type = str) # directory of the parquet gzip files
     parser.add_argument("file_part_max_observations", help = "maximum file part number for observations", type = int) # maximum file part number for observations
     parser.add_argument("file_part_max_clinical", help = "maximum file part number for clinical", type = int) # maximum file part number for clinical
     args = parser.parse_args()
 
-    merge_clean_notes(args.data_dir_observations, args.data_dir_clinical, 
-                      args.json_dir_observations, args.json_dir_clinical,
-                      args.mrn_file, args.save_dir, args.file_part_max_observations, 
+    merge_clean_notes(args.parquet_gzip_dir, args.file_part_max_observations, 
                       args.file_part_max_clinical)
