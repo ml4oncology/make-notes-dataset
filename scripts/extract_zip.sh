e2c96ba8cc9592a5bf7757b74dd434601dcfb6ae
wayneisaacuy <wayneisaacuy@gmail.com>
2024-11-13 17:49:24 -0500

    clean up file names

--- scripts/extract_zip.sh
+++ scripts/extract_zip.sh
@@ -10,22 +10,22 @@ partition="all"
 
 data_dir=/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04
 
-zip_dir=${data_dir}/zip_json_p1
-output_dir=${data_dir}/raw_json_p1
+zip_dir=${data_dir}/zip_json_observations
+output_dir=${data_dir}/raw_json_observations
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_json_p2
-output_dir=${data_dir}/raw_json_p2
+zip_dir=${data_dir}/zip_json_clinic
+output_dir=${data_dir}/raw_json_clinic
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_notes_p1
-output_dir=${data_dir}/raw_parquet_gzip_notes_p1
+zip_dir=${data_dir}/zip_notes_observations
+output_dir=${data_dir}/raw_parquet_gzip_notes_observations
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_notes_p2
-output_dir=${data_dir}/raw_parquet_gzip_notes_p2
+zip_dir=${data_dir}/zip_notes_clinic
+output_dir=${data_dir}/raw_parquet_gzip_notes_clinic
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
