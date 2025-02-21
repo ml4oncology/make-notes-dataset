7b9af7d49b32695a4d444994ada54a6e79cf0112
wayneisaacuy <wayneisaacuy@gmail.com>
2025-02-21 23:38:13 +0800

    update directory names

--- scripts/extract_zip.sh
+++ scripts/extract_zip.sh
@@ -10,22 +10,22 @@ partition="all"
 
 data_dir=/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04
 
-zip_dir=${data_dir}/zip_json_observations
-output_dir=${data_dir}/raw_json_observations
+zip_dir=${data_dir}/observation_json_zip
+output_dir=${data_dir}/observation_json
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_json_clinic
-output_dir=${data_dir}/raw_json_clinic
+zip_dir=${data_dir}/clinic_notes_json_zip
+output_dir=${data_dir}/clinic_notes_json
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_notes_observations
-output_dir=${data_dir}/raw_parquet_gzip_notes_observations
+zip_dir=${data_dir}/observation_csv_zip
+output_dir=${data_dir}/observation_parquet
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
 
-zip_dir=${data_dir}/zip_notes_clinic
-output_dir=${data_dir}/raw_parquet_gzip_notes_clinic
+zip_dir=${data_dir}/clinic_notes_csv_zip
+output_dir=${data_dir}/clinic_notes_parquet
 
 pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/extract_zip.py $zip_dir $output_dir" 
--- scripts/process_notes.sh
+++ scripts/process_notes.sh
@@ -12,8 +12,8 @@ partition="all"
 save_dir="/cluster/projects/gliugroup/2BLAST/data/processed/clinical_notes/data_pull_2024-06-04"
 mrn_file="/cluster/home/t127556uhn/misc/mrn_map_2Blast_part4.csv"
 
-data_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_parquet_gzip_notes_observations"
-json_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_json_observations"
+data_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/observation_parquet"
+json_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/observation_json"
 clinic_notes=0
 upper_limit=598
 for ((i=0; i<=upper_limit; i++))
@@ -21,8 +21,8 @@ do
     pySLURMargs.py $userName $memory $condaEnv $nGPU $run_time $partition "../src/process_notes.py $data_dir $json_dir $save_dir $mrn_file $clinic_notes $i"
 done
 
-data_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_parquet_gzip_notes_clinic"
-json_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/raw_json_clinic"
+data_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/clinic_notes_parquet"
+json_dir="/cluster/projects/gliugroup/2BLAST/data/raw/data_pull_2024-06-04/clinic_notes_json"
 clinic_notes=1
 upper_limit=598
 for ((i=0; i<=upper_limit; i++))
